18819065461e8a24e5377e6bf8871176f13bbfdb
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-06-14 21:50:46 +0530

    Removed Test-Bard

--- src/components/SettingsModal.js
+++ src/components/SettingsModal.js
@@ -42,7 +42,6 @@ const SettingsModal = ({ isOpen, onClose, setSettingOptions, settingOptions, set
                             <option value="gpt-3.5-turbo">GPT 3.5 Turbo</option>
                             <option value="gpt-3.5-turbo-16k">GPT 3.5 Turbo - 16K</option>
                             <option value="gpt-4">GPT 4</option>
-                            <option value="test-bard">Test Bard</option>
                         </Form.Select>
                     </FloatingLabel>
                 </div>
